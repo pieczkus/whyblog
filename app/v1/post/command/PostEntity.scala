5a1f281e78f7cd2bb6df7576f4b3a2d839b87101
Bartłomiej Pieczka <bartlomiej.pieczka@gmail.com>
2017-05-09 05:57:26 +0200

    work in progress

---
+++ app/v1/post/command/PostEntity.scala
@@ -0,0 +1,164 @@
+package v1.post.command
+
+import java.util.Date
+
+import akka.actor.Props
+import com.trueaccord.scalapb.GeneratedMessage
+import pl.why.common._
+import pl.why.blog.proto.Blog
+import v1.post.command.PostEntity.Command.{AddRelatedPost, CreatePost, PublishPost}
+import v1.post.command.PostEntity.Event.{PostCreated, PostPublished, RelatedPostAdded}
+
+case class BodyComponentData(component: String, parameters: Map[String, String])
+
+case class PostData(postId: String, key: String, author: String, title: String, body: Seq[BodyComponentData], coverUrl: String,
+                    metaTitle: String, metaDescription: String, metaKeywords: String, publishedOn: Long = 0L,
+                    commentCount: Int = 0, timeToRead: String = "", tags: List[String] = List.empty,
+                    relatedPosts: Seq[String] = List.empty, deleted: Boolean = false) extends EntityFieldsObject[String, PostData] {
+  def assignId(id: String): PostData = this.copy(postId = id)
+
+  def id: String = postId
+
+  def markDeleted: PostData = this.copy(deleted = true)
+}
+
+object PostData {
+  val empty = PostData("", "", "", "", Seq.empty, "", "", "", "")
+}
+
+object PostEntity {
+  val EntityType = "post"
+
+  case class UpdatePostInput(body: String, publishedOn: Long, coverUrl: String, tags: List[String])
+
+  object Command {
+
+    case class CreatePost(post: PostData) extends EntityCommand {
+      def entityId: String = post.postId
+    }
+
+    case class PublishPost(id: String) extends EntityCommand {
+      def entityId: String = id
+    }
+
+    case class UpdateTimeToRead(timeToRead: String, id: String) extends EntityCommand {
+      def entityId: String = id
+    }
+
+    case class AddRelatedPost(relatedPostId: String, id: String) extends EntityCommand {
+      def entityId: String = id
+    }
+
+  }
+
+  object Event {
+
+    trait PostEvent extends EntityEvent {
+      def entityType = EntityType
+    }
+
+    case class PostCreated(p: PostData) extends PostEvent {
+      override def toDataModel: Blog.PostCreated = {
+
+        val body = p.body.map(b => Blog.BodyComponent(b.component, b.parameters))
+
+        val post = Blog.Post(p.postId, p.key, p.author, p.timeToRead, body, p.coverUrl, p.metaTitle, p.metaDescription, p.metaKeywords,
+          p.publishedOn, p.commentCount, p.timeToRead, p.tags, p.relatedPosts, p.deleted)
+        Blog.PostCreated(Some(post))
+      }
+    }
+
+    object PostCreated extends DataModelReader {
+      def fromDataModel: PartialFunction[GeneratedMessage, PostCreated] = {
+        case dm: Blog.PostCreated =>
+          val p = dm.getPost
+          val body = p.body.map(b => BodyComponentData(b.component, b.parameters))
+          PostCreated(PostData(p.postId, p.key, p.author, p.timeToRead, body, p.coverUrl, p.metaTitle, p.metaDescription, p.metaKeywords,
+            p.publishedOn, p.commentCount, p.timeToRead, p.tags.toList, p.relatedPosts, p.deleted))
+      }
+    }
+
+    case class PostPublished(publishedOn: Long) extends PostEvent {
+      override def toDataModel: Blog.PostPublished = {
+        Blog.PostPublished(publishedOn)
+      }
+    }
+
+    object PostPublished extends DataModelReader {
+      override def fromDataModel: PartialFunction[GeneratedMessage, PostPublished] = {
+        case dm: Blog.PostPublished =>
+          PostPublished(dm.publishedOn)
+      }
+    }
+
+    case class TimeToReadUpdated(timeToRead: String) extends PostEvent {
+      override def toDataModel: Blog.TimeToReadUpdated = {
+        Blog.TimeToReadUpdated(timeToRead)
+      }
+    }
+
+    object TimeToReadUpdated extends DataModelReader {
+      override def fromDataModel: PartialFunction[GeneratedMessage, TimeToReadUpdated] = {
+        case dm: Blog.TimeToReadUpdated =>
+          TimeToReadUpdated(dm.timeToRead)
+      }
+    }
+
+    case class RelatedPostAdded(relatedPostIds: Seq[String]) extends PostEvent {
+      override def toDataModel: Blog.RelatedPostAdded = {
+        Blog.RelatedPostAdded(relatedPostIds)
+      }
+    }
+
+    object RelatedPostAdded extends DataModelReader {
+      override def fromDataModel: PartialFunction[GeneratedMessage, RelatedPostAdded] = {
+        case dm: Blog.RelatedPostAdded =>
+          RelatedPostAdded(dm.relatedPostIds)
+      }
+    }
+
+  }
+
+  def props: Props = Props[PostEntity]
+}
+
+class PostEntity extends PersistentEntity[PostData] {
+
+  override def additionalCommandHandling: Receive = {
+    case CreatePost(post) =>
+      log.info(s"Post tags: ${post.tags}")
+      persist(PostCreated(post)) {
+        handleEventAndRespond()
+      }
+
+    case PublishPost(_) =>
+      log.info(s"Publishing post ${state.title}")
+      persist(PostPublished(new Date().getTime)) {
+        handleEventAndRespond()
+      }
+
+    case AddRelatedPost(relatedTitle, _) =>
+      persist(RelatedPostAdded(state.relatedPosts :+ relatedTitle)) {
+        handleEventAndRespond()
+      }
+  }
+
+  override def isCreateMessage(cmd: Any): Boolean = cmd match {
+    case CreatePost(_) => true
+    case _ => false
+  }
+
+  override def initialState: PostData = PostData.empty
+
+  override def handleEvent(event: EntityEvent): Unit = event match {
+    case PostCreated(post) =>
+      log.info(s"Post Event tags: ${post.tags}")
+      state = post
+
+    case PostPublished(publishedOn) =>
+      state = state.copy(publishedOn = publishedOn)
+
+    case RelatedPostAdded(relatedPostIds) =>
+      state = state.copy(relatedPosts = relatedPostIds)
+  }
+}
---
+++ app/v1/post/command/PostManager.scala
@@ -0,0 +1,78 @@
+package v1.post.command
+
+import akka.actor.Props
+import akka.util.Timeout
+import pl.why.common.PersistentEntity.GetState
+import pl.why.common._
+import v1.post.command.PostEntity.Command.{AddRelatedPost, CreatePost}
+
+import scala.concurrent.duration._
+
+
+object PostManager {
+  val Name = "posts-manager"
+
+  case class FindPostById(id: String)
+
+  case class AddPostInput(key: String, author: String, title: String, body: Seq[BodyComponentData], coverUrl: String, tags: List[String],
+                          metaTitle: String, metaDescription: String, metaKeywords: String)
+
+  case class AddPost(input: AddPostInput)
+
+  val TitleNotUniqueError = ErrorMessage("post.title.nonunique", Some("The post title supplied for a create is not unique"))
+
+  def props: Props = Props[PostManager]
+
+}
+
+class PostManager extends Aggregate[PostData, PostEntity] {
+
+  import PostManager._
+  import akka.pattern.ask
+  import context.dispatcher
+
+  override def entityProps: Props = PostEntity.props
+
+  override def receive: Receive = {
+    case FindPostById(id) =>
+      forwardCommand(id, GetState(id))
+
+    case AddPost(input) =>
+      implicit val timeout = Timeout(5.seconds)
+
+      val id = Math.abs(input.title.hashCode).toString
+
+      val stateFut = (entityShardRegion ? GetState(id)).mapTo[ServiceResult[PostData]]
+      val caller = sender()
+      stateFut onComplete {
+        case util.Success(FullResult(_)) =>
+          caller ! Failure(FailureType.Validation, TitleNotUniqueError)
+
+        case util.Success(EmptyResult) =>
+          val fo = PostData(id, input.key, input.author, input.title, input.body, input.coverUrl, input.metaTitle, input.metaDescription,
+            input
+            .metaKeywords, tags = input.tags, timeToRead = calculateTieToRead(input.body))
+          entityShardRegion.tell(CreatePost(fo), caller)
+
+        case _ =>
+          caller ! Failure(FailureType.Service, ServiceResult.UnexpectedFailure)
+      }
+
+    case pp: PublishPost =>
+      forwardCommand(pp.id, pp)
+
+    case arp: AddRelatedPost =>
+      forwardCommand(arp.id, arp)
+  }
+
+  private def calculateTieToRead(body: Seq[BodyComponentData]): String = {
+    val minutes = body.filter(bc => bc.parameters.contains("text")).map(bc => bc.parameters("key").split("\\W+").length).sum
+    if (minutes <= 1) {
+      "1 Minuta"
+    } else if (minutes > 10) {
+      "10+ Minut"
+    } else {
+      minutes + " Minuty"
+    }
+  }
+}
